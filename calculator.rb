cf5bd407e17dd4f5b8334eb961f020effa48b668
Victoria Hale <vic.hale74@gmail.com>
2019-09-04 03:07:24 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,6 +1,6 @@
 first_number = 2.0 
 second_number = 1.0
 sum = first_number + second_number
-difference = second_number - first_number
+difference = first_number - second_number
 product = first_number * second_number
 quotient = second_number / first_number
